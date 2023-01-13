fc4c04cfc1d76b3cdd53322f5ffb7e78b5013aca
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-01-13 12:28:47 +0100

    feat: setup layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import { useState } from "react";
+import { Center, Heading } from "@chakra-ui/react";
+import Player from "./Player";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Center w="full" h="100vh" flexDir="column">
+      <Heading mb="10">Projet XState</Heading>
+
+      <Player />
+    </Center>
   );
 }
 
