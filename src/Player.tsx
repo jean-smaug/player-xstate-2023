5628f5bd1e7d078a4c9791e8eda4db3d91d110a6
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-01-13 14:57:40 +0100

    feat: setup basic player component

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { useMachine } from "@xstate/react";
+import { Box } from "@chakra-ui/react";
+
+import video from "./assets/pursuit-of-happiness.mp4";
 import { playerMachine } from "./player.fsm";
 
 const Player = () => {
   // Récupération de la machine à état
   const [playerState, playerSend] = useMachine(playerMachine);
 
+  // Récupération de la balise video
+  const videoRef = React.useRef(null);
+  const videoElement = videoRef.current;
+
   // Réactions aux changements d'états
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {}, [playerState]);
 
   // Rendu visuel
-  return <div>Player</div>;
+  return (
+    <Box position="relative">
+      <video
+        style={{
+          height: "50vh",
+        }}
+        loop={false}
+        ref={videoRef}
+        src={video}
+      ></video>
+    </Box>
+  );
 };
 
 export default Player;
