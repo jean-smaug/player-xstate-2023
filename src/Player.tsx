2d942d7b0718533801b77d30afc88a30a90138f2
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-01-13 12:38:37 +0100

    feat: add comments to describe player component

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const Player = () => {
+  // Récupération de la machine à état
+
+  // Réactions aux changements d'états
+  React.useEffect(() => {}, []);
+
+  // Rendu visuel
   return <div>Player</div>;
 };
 
